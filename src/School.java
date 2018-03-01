ea29464100828fd5d24a616eb2a1cb7f5e7ff3b2
Luis Adame <mrluissan@gmail.com>
2018-03-01 01:41:33 +0100

    Refactor byGrade function

--- src/School.java
+++ src/School.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class School {
 
     public ArrayList<ArrayList<Student>> sortByGrade(double grade) {
         ArrayList<ArrayList<Student>> sortedStudents = new ArrayList<>();
-        ArrayList<Student> above = new ArrayList<>();
-        ArrayList<Student> below = new ArrayList<>();
 
-        for (Student student : Student.students) {
-            if(student.getGrade() < grade) {
-                below.add(student);
-            } else if (student.getGrade() > grade) {
-                above.add(student);
-            }
-        }
+        ArrayList<Student> above = Student.students.stream()
+                                    .filter(student -> student.getGrade() < grade)
+                                    .collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Student> below = Student.students.stream()
+                                    .filter(student -> student.getGrade() > grade)
+                                    .collect(Collectors.toCollection(ArrayList::new));
 
         sortedStudents.add(above);
         sortedStudents.add(below);
