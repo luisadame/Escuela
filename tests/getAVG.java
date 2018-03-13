1f3f419afa1a5fe5cd33aa8dadb37ac49bf86fe4
Luis Adame <mrluissan@gmail.com>
2018-03-13 12:04:38 +0100

    Refactor testGetAvg with e. partitions and limit values techniques #10

--- tests/getAVG.java
+++ tests/getAVG.java
@@ -1,16 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+/**
+ * A valid grade is considered to be between 0 and 10.
+ * With a maximum of two decimal points for the grade.
+ * Use of equivalent partitions and limit values techniques.
+ */
 public class getAVG extends StudentTest {
 
+    double min = 0;
+    double max = 10;
+
     @Test
-    void testGetAvg() {
-        Student k = new Student();
-        k.setAVG(7.0);
-        assertEquals(7.0, k.getAVG());
-        assertNotEquals(7.1, k.getAVG());
-        assertNotEquals(6.9, k.getAVG());
+    void testValidValues() {
+        luis.setAVG(min);
+        assertEquals(min, luis.getAVG());
+        luis.setAVG(max);
+        assertEquals(max, luis.getAVG());
+    }
+
+    @Test
+    void testInferiorMinusOneValue() {
+        luis.setAVG(min - 1);
+        assertNotEquals(min - 1, luis.getAVG());
+    }
+
+    @Test
+    void testSuperiorPlusOneValue() {
+        luis.setAVG(max + 1);
+        assertNotEquals(max + 1, luis.getAVG());
     }
 }
