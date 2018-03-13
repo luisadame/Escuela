2ce34f9a73ee39778519e20535d1381fc88230cc
Luis Adame <mrluissan@gmail.com>
2018-03-13 13:20:04 +0100

    Test all the possible conditions that can be given in isLeveledHighSchool #10

--- tests/isLeveledHighSchool.java
+++ tests/isLeveledHighSchool.java
@@ -15,7 +15,7 @@ public class isLeveledHighSchool extends SchoolTest {
     }
 
     @Test
-    void withNoStudents() {
+    void testWithNoStudents() {
         assertFalse(school.isLeveledHighSchool());
     }
 
@@ -26,7 +26,7 @@ public class isLeveledHighSchool extends SchoolTest {
      * and if there are only prepared students it returns true.
      */
     @Test
-    void withOneStudent() {
+    void testWithOneStudent() {
         Student k = new Student();
         k.setAVG(5.0);
         assertFalse(school.isLeveledHighSchool());
@@ -34,9 +34,14 @@ public class isLeveledHighSchool extends SchoolTest {
         assertTrue(school.isLeveledHighSchool());
     }
 
+    /**
+     * This test is actually the one that is testing the difference in grades
+     * and calculating if it is a leveledHighSchool
+     */
     @Test
-    void withManyStudents() {
+    void testWithManyStudents() {
         // Make a new set of students that are all above 5.
+        // the difference in grades is less than 2
         for (int i = 0; i < 10; i++) {
             Student k = new Student();
             if( i <=4 ) {
@@ -48,6 +53,7 @@ public class isLeveledHighSchool extends SchoolTest {
         assertTrue(school.isLeveledHighSchool());
         Student.students.clear();
         // Make a new set of students that are all above 5.
+        // the difference in grades is greater than 2
         for (int i = 0; i < 10; i++) {
             Student k = new Student();
             if( i <=4 ) {
@@ -57,5 +63,29 @@ public class isLeveledHighSchool extends SchoolTest {
             }
         }
         assertFalse(school.isLeveledHighSchool());
+    }
+
+    /**
+     * Create a set of students who are not prepared.
+     */
+    @Test
+    void testWithNoPreparedStudents() {
+        for (int i = 0; i < 10; i++) {
+            Student k = new Student();
+            k.setAVG(6.25);
+        }
+        assertFalse(school.isLeveledHighSchool());
+    }
+
+    /**
+     * Create a set of students who are prepared.
+     */
+    @Test
+    void testWithPreparedStudents() {
+        for (int i = 0; i < 10; i++) {
+            Student k = new Student();
+            k.setAVG(7.25);
+        }
+        assertTrue(school.isLeveledHighSchool());
     }
 }
